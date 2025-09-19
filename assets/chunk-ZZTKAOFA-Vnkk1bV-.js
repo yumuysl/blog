9751c66f6fb952bbb199d6633c669fc0169a4ed9
yumuysl <yinchuanyjw@outlook.com>
2025-09-20 00:34:31 +0800

    feat: 新增音乐播放器

---
+++ assets/chunk-ZZTKAOFA-Vnkk1bV-.js
@@ -0,0 +1,15 @@
+import{m as e}from"./mermaid.esm.min-BK7i6U7o.js";var l=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as o};
