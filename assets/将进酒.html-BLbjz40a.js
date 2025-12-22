1317ff546e993a56f0a9a6cd2e2cdd950b704f2e
yumuysl <yinchuanyjw@outlook.com>
2025-12-23 00:04:10 +0800

    更新

---
+++ assets/将进酒.html-BLbjz40a.js
@@ -0,0 +1,25 @@
+import{_ as s}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as a,f as e,o as i}from"./app-CMzfx7vU.js";const l={};function p(t,n){return i(),a("div",null,[...n[0]||(n[0]=[e(`<ul><li>作者：李白</li><li>全诗如下</li></ul><div class="language- line-numbers-mode" data-highlighter="shiki" data-ext="" style="--shiki-light:#383A42;--shiki-dark:#abb2bf;--shiki-light-bg:#FAFAFA;--shiki-dark-bg:#282c34;"><pre class="shiki shiki-themes one-light one-dark-pro vp-code"><code class="language-"><span class="line"><span>君不见黄河之水天上来，奔流到海不复回。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>君不见高堂明镜悲白发，朝如青丝暮成雪。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>人生得意须尽欢，莫使金樽空对月。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>天生我材必有用，千金散尽还复来。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>烹羊宰牛且为乐，会须一饮三百杯。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>岑夫子，丹丘生，将进酒，杯莫停。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>与君歌一曲，请君为我倾耳听。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>钟鼓馔玉不足贵，但愿长醉不复醒。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>古来圣贤皆寂寞，惟有饮者留其名。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>陈王昔时宴平乐，斗酒十千恣欢谑。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>主人何为言少钱，径须沽取对君酌。</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>五花马，千金裘，呼儿将出换美酒，</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>与尔同销万古愁。</span></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2)])])}const d=s(l,[["render",p]]),o=JSON.parse('{"path":"/livelog/poetry/libai/%E5%B0%86%E8%BF%9B%E9%85%92.html","title":"将进酒","lang":"zh-CN","frontmatter":{"title":"将进酒","tag":"诗词","description":"我喜欢的诗：将进酒-李白","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"将进酒\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-10-07T13:51:47.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"禹穆穆\\"}]}"],["meta",{"property":"og:url","content":"https://bestoaloe.gitee.io/livelog/poetry/libai/%E5%B0%86%E8%BF%9B%E9%85%92.html"}],["meta",{"property":"og:site_name","content":"禹穆穆"}],["meta",{"property":"og:title","content":"将进酒"}],["meta",{"property":"og:description","content":"我喜欢的诗：将进酒-李白"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-10-07T13:51:47.000Z"}],["meta",{"property":"article:tag","content":"诗词"}],["meta",{"property":"article:modified_time","content":"2024-10-07T13:51:47.000Z"}]]},"git":{"createdTime":1710066825000,"updatedTime":1728309107000,"contributors":[{"name":"bestoaloe","username":"","email":"yinchuanyjw@outlook.com","commits":6}]},"readingTime":{"minutes":0.69,"words":206},"filePathRelative":"livelog/poetry/libai/将进酒.md","excerpt":""}');export{d as comp,o as data};
